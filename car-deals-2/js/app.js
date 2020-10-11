c0bdb529fe4eed9bdf2a7f3c322134cd19b15b03
Bill Stavroulakis <bill.stavroulakis@getcruise.com>
2020-10-10 18:03:56 -0700

    module 1

--- car-deals-2/js/app.js
+++ car-deals-2/js/app.js
@@ -1,11 +1,8 @@
 import { loadCarPage } from "./carPageService.js";
 import { loadCars } from "./carService.js";
-import swRegister from "./swRegister.js";
 
 window.pageEvents = {
   loadCarPage,
-  loadCars,
 };
 
 loadCars();
-swRegister();
--- car-deals-2/js/carService.js
+++ car-deals-2/js/carService.js
@@ -1,37 +1,8 @@
-import { preCacheDetailsPage } from "./carPageService.js";
-import { addCars, getCars, getLastItemId } from "./clientStorage.js";
 import { API_URL_LATEST } from "./constants.js";
 import { appendCars } from "./template.js";
 
 export const loadCars = async () => {
-  // Fetch, update cache and set status
-  document.getElementById("connection-status").innerHTML = await fetchPromise();
-  // Load cars from cache
-  const cars = await getCars();
-  // Append cars to HTML
-  appendCars(cars);
-};
-
-const fetchPromise = () => {
-  const promiseRequest = new Promise(async (resolve) => {
-    try {
-      await loadCarsRequest();
-    } catch (err) {
-      resolve("No connection, showing offline results");
-    }
-    resolve("The connection is OK, showing latest results");
-  });
-
-  const promiseHanging = new Promise((resolve) =>
-    setTimeout(resolve, 3000, "The connection is hanging, showing offline results")
-  );
-  return Promise.race([promiseRequest, promiseHanging]);
-};
-
-const loadCarsRequest = async () => {
-  const requestURL = `${API_URL_LATEST}?carId=${await getLastItemId()}`;
-  const response = await fetch(requestURL);
+  const response = await fetch(API_URL_LATEST);
   const data = await response.json();
-  await addCars(data.cars);
-  data.cars.forEach(preCacheDetailsPage);
+  appendCars(data.cars);
 };
